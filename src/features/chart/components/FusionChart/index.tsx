a06af5ae8382adf32f50d175c608bc7ae12cffd1
yookyoung-park <yookyoung.park@growinglab.team>
2024-08-25 03:33:36 +0900

    feat: fusionchart initialize

--- src/features/chart/components/FusionChart/index.tsx
+++ src/features/chart/components/FusionChart/index.tsx
@@ -1,5 +1,42 @@
+import FusionCharts from "fusioncharts";
+import Charts from "fusioncharts/fusioncharts.charts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
+import ReactFC from "react-fusioncharts";
+
+ReactFC.fcRoot(FusionCharts, Charts, FusionTheme);
+
+const dataSource = {
+  chart: {
+    caption: "Countries With Most Oil Reserves [2017-18]",
+    subCaption: "In MMbbl = One Million barrels",
+    xAxisName: "Country",
+    yAxisName: "Reserves (MMbbl)",
+    numberSuffix: "K",
+    theme: "fusion",
+  },
+  data: [
+    { label: "Venezuela", value: "290" },
+    { label: "Saudi", value: "260" },
+    { label: "Canada", value: "180" },
+    { label: "Iran", value: "140" },
+    { label: "Russia", value: "115" },
+    { label: "UAE", value: "100" },
+    { label: "US", value: "30" },
+    { label: "China", value: "30" },
+  ],
+};
+
+const chartConfigs = {
+  type: "column2d",
+  width: 600,
+  height: 400,
+  dataFormat: "json",
+  dataSource: dataSource,
+};
+
 function FusionChart() {
-  return <div>차트</div>;
+  // @ts-expect-error react-fusionchart에서 타입 설정이 이상하게 되어 있음
+  return <ReactFC {...chartConfigs} />;
 }
 
 export default FusionChart;
